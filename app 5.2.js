56ef7a2bba298ff75fa20890b31b1abeab2fa077
testknight <preetham.samuel@gmail.com>
2016-12-20 20:37:24 +0530

    Add files via upload

---
+++ app 5.2.js
@@ -0,0 +1,8 @@
+var person = {
+    firstName : "John",
+    lastName  : "Wick",
+    age       : 55,
+    dob		  : 19081960,
+    birthPlace: "Madras",
+};
+console.log(person["firstName"] + " " + person["lastName"] + " "+ person["age"] +" " + person["dob"] + " " + person["birthPlace"]);
