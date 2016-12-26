b76763d8ff15da7d1948de7bf4e93984b3a2f97d
testknight <preetham.samuel@gmail.com>
2016-12-26 10:35:07 +0530

    Add files via upload

---
+++ app 6.4.js
@@ -0,0 +1,12 @@
+var txt = "";
+var Employee = [
+				[ "Abbey", 32 ,13000, "banglore", "Karnataka", 900002], 
+				["Bobby", 25, 8000, "Delhi", "Union Territory" , 2300045], 
+				["Danny", 29, 10000, "Punjab", "Chandigarh", 3000056 ], 
+				["Linda", 38, 20000, "Mumbai", "Maharastra", 4000067]
+				];
+var x;				
+for (x in Employee){
+	txt += Employee[x]+"";
+}  
+console.log(Employee) = txt;
